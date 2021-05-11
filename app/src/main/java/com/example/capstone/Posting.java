b70af7bf374322ebbdc12280b46bde88f417619b
Brian <BrianM0330@gmail.com>
2021-05-11 15:46:11 -0500

    refactor

--- app/src/main/java/com/example/capstone/Posting.java
+++ app/src/main/java/com/example/capstone/Posting.java
@@ -2,7 +2,7 @@ package com.example.capstone;
 
 // https://stackoverflow.com/questions/22288659/how-can-i-parse-pdf-file-in-android
 
-public class JobPosting {
+public class Posting {
     private String jobTitle;
     private String[] qualifications;
 }
