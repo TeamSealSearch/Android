98921e1a3d1ed36b68ac84451a7114211a83fabe
Brian <BrianM0330@gmail.com>
2021-05-11 15:19:48 -0500

    skeleton job posting class

---
+++ app/src/main/java/com/example/capstone/JobPosting.java
@@ -0,0 +1,8 @@
+package com.example.capstone;
+
+// https://stackoverflow.com/questions/22288659/how-can-i-parse-pdf-file-in-android
+
+public class JobPosting {
+    private String jobTitle;
+    private String[] qualifications;
+}
