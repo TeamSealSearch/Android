24abbe6735f55ae39dad005f9f929a4e515de387
Brian <BrianM0330@gmail.com>
2021-05-11 15:46:41 -0500

    applicant recyclerview scaffold

---
+++ app/src/main/java/com/example/capstone/ApplicantAdapter.java
@@ -0,0 +1,24 @@
+package com.example.capstone;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ApplicantAdapter extends RecyclerView.Adapter<ApplicantViewHolder> {
+
+    @Override
+    public ApplicantViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ApplicantViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/capstone/ApplicantViewHolder.java
@@ -0,0 +1,13 @@
+package com.example.capstone;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ApplicantViewHolder extends RecyclerView.ViewHolder {
+
+    public ApplicantViewHolder(View view) {
+        super(view);
+    }
+
+}
