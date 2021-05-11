061b1786aad8984a1364455f52f3cdd2da590f1e
Brian <BrianM0330@gmail.com>
2021-05-11 14:46:08 -0500

    scaffold login screen

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.capstone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginScreen";
+
+    private EditText usernameField;
+    private EditText passwordField;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameField = findViewById(R.id.loginUsername);
+        passwordField = findViewById(R.id.loginPassword);
+        loginButton = findViewById(R.id.loginButton);
+
+
     }
 }
