db8fca5f0e7c2fad05f944e95f825ee1b8d68573
Brian <BrianM0330@gmail.com>
2021-05-11 15:46:32 -0500

    posting recyclerview scaffold

---
+++ app/src/main/java/com/example/capstone/PostingAdapter.java
@@ -0,0 +1,24 @@
+package com.example.capstone;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PostingAdapter extends RecyclerView.Adapter<PostingViewHolder> {
+
+    @Override
+    public PostingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostingViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/capstone/PostingViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.capstone;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PostingViewHolder extends RecyclerView.ViewHolder {
+
+    public PostingViewHolder(View view) {
+        super(view);
+    }
+
+}
