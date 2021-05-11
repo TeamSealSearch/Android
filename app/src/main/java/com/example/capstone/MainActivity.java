25176255004d2acb2aac1be0b35efc5074e40ed1
Brian <BrianM0330@gmail.com>
2021-05-11 15:37:59 -0500

    added employer/applicant toggle

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -2,17 +2,23 @@ package com.example.capstone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "LoginScreen";
+    private boolean employerMode = false;
+    private String toggleFormatString = "Switch to %s";
 
     private EditText usernameField;
     private EditText passwordField;
     private Button loginButton;
+    private Button createButton;
+    private Button toggleModeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,16 @@ public class MainActivity extends AppCompatActivity {
         usernameField = findViewById(R.id.loginUsername);
         passwordField = findViewById(R.id.loginPassword);
         loginButton = findViewById(R.id.loginButton);
+        createButton = findViewById(R.id.loginCreateButton);
+        toggleModeButton = findViewById(R.id.loginToggleButton);
+    }
 
+    public void toggleMode(View v) {
+        employerMode = !employerMode;
 
+        if (employerMode)
+            findViewById(R.id.mainConstraint).setBackgroundColor(Color.LTGRAY);
+        else
+            findViewById(R.id.mainConstraint).setBackgroundColor(Color.BLUE);
     }
 }
