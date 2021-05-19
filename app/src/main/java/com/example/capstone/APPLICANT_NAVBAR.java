0d7dc7192385bbd1f00d09b5cc59be83274ce67f
Brian <BrianM0330@gmail.com>
2021-05-19 16:14:44 -0500

    enum values added

--- app/src/main/java/com/example/capstone/APPLICANT_NAVBAR.java
+++ app/src/main/java/com/example/capstone/APPLICANT_NAVBAR.java
@@ -1,4 +1,8 @@
 package com.example.capstone;
 
 public enum APPLICANT_NAVBAR {
+    Browse_Jobs,
+    Modify_Filters,
+    Modify_Resume,
+    Followed_Companies
 }
--- app/src/main/java/com/example/capstone/EMPLOYER_NAVBAR.java
+++ app/src/main/java/com/example/capstone/EMPLOYER_NAVBAR.java
@@ -1,4 +1,10 @@
 package com.example.capstone;
 
 public enum EMPLOYER_NAVBAR {
+    Create_Jobs,
+    Edit_Current_Jobs,
+    View_Applied_Candidates,
+    Approved_Candidates,
+    Hibernate_Job_Listings,
+    Company_Filters
 }
