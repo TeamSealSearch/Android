ccf10a8575eaadb477f6d17590410384893f356f
Brian <BrianM0330@gmail.com>
2021-05-28 15:13:07 -0500

    animations added

--- app/src/main/java/com/example/capstone/CompanyProfileAdapter.java
+++ app/src/main/java/com/example/capstone/CompanyProfileAdapter.java
@@ -1,5 +1,8 @@
 package com.example.capstone;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,9 +12,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+//Animations from https://betterprogramming.pub/android-recyclerview-with-beautiful-animations-5e9b34dbb0fa
+
 public class CompanyProfileAdapter extends RecyclerView.Adapter<CompanyProfileViewHolder> {
     ArrayList<CompanyProfile> companies;
     followedCompanies main;
+    long DURATION = 250;
+    private boolean on_attach = true;
 
     public CompanyProfileAdapter(ArrayList<CompanyProfile> c, followedCompanies frag) {
         companies = c;
@@ -31,10 +38,47 @@ public class CompanyProfileAdapter extends RecyclerView.Adapter<CompanyProfileVi
     public void onBindViewHolder(@NonNull CompanyProfileViewHolder holder, int position) {
         holder.companyName.setText(companies.get(position).getName());
         holder.industry.setText(companies.get(position).getIndustry());
+
+        setAnimation(holder.itemView, position);
     }
 
     @Override
     public int getItemCount() {
         return companies.size();
     }
+    
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                on_attach = false;
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+        });
+
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    private void setAnimation(View itemView, int i) {
+        if (!on_attach){
+            i = -1;
+        }
+        boolean not_first_item = i == -1;
+        i = i + 1;
+
+        itemView.setTranslationX(itemView.getX() + 1200);
+        itemView.setAlpha(0.f);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        ObjectAnimator animatorTranslateY = ObjectAnimator.ofFloat(itemView, "translationX", itemView.getX() + 400, 0);
+        ObjectAnimator animatorAlpha = ObjectAnimator.ofFloat(itemView, "alpha", 1.f);
+        ObjectAnimator.ofFloat(itemView, "alpha", 0.f).start();
+        animatorTranslateY.setStartDelay(not_first_item ? DURATION : (i * DURATION));
+        animatorTranslateY.setDuration((not_first_item ? 2 : 1) * DURATION);
+        animatorSet.playTogether(animatorTranslateY, animatorAlpha);
+        animatorSet.start();
+    }
+    
 }
--- app/src/main/java/com/example/capstone/PostingAdapter.java
+++ app/src/main/java/com/example/capstone/PostingAdapter.java
@@ -1,5 +1,7 @@
 package com.example.capstone;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +17,8 @@ public class PostingAdapter extends RecyclerView.Adapter<PostingViewHolder> {
     Context context;
     ArrayList<Posting> postings;
     private browseJobs_applicantMain main;
+    private long DURATION = 200;
+    private boolean on_attach = true;
 
     public PostingAdapter (ArrayList<Posting> p, browseJobs_applicantMain frag) {
         postings = p;
@@ -36,6 +40,38 @@ public class PostingAdapter extends RecyclerView.Adapter<PostingViewHolder> {
         holder.jobTitle.setText(postings.get(position).getJobTitle());
         holder.company.setText(postings.get(position).getCompany());
         holder.qualifications.setText(postings.get(position).getQualifications());
+
+        setAnimation(holder.itemView, position);
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                on_attach = false;
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+        });
+
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    private void setAnimation(View itemView, int i) {
+        if (!on_attach) {
+            i = -1;
+        }
+        boolean isNotFirstItem = i == -1;
+        i++;
+        itemView.setAlpha(0.f);
+        AnimatorSet animatorSet = new AnimatorSet();
+        ObjectAnimator animator = ObjectAnimator.ofFloat(itemView, "alpha", 0.f, 0.5f, 1.0f);
+        ObjectAnimator.ofFloat(itemView, "alpha", 0.f).start();
+        animator.setStartDelay(isNotFirstItem ? DURATION / 2 : (i * DURATION / 3));
+        animator.setDuration(500);
+        animatorSet.play(animator);
+        animator.start();
     }
 
     @Override
