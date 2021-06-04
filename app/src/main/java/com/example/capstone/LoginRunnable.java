9eb196232d259d784c28dfbcfb145dfcab14e496
Brian <BrianM0330@gmail.com>
2021-06-04 15:21:49 -0500

    dynamic queries for employer table

--- app/src/main/java/com/example/capstone/LoginRunnable.java
+++ app/src/main/java/com/example/capstone/LoginRunnable.java
@@ -11,20 +11,29 @@ import java.sql.Statement;
 public class LoginRunnable implements Runnable {
 
     private MainActivity main;
-    private String QUERY = "SELECT * FROM %s WHERE a_username = '%s'"; //query for finding users with matching uName
+    private String QUERY = "SELECT * FROM %s WHERE %s = '%s'"; //query for finding users with matching uName
     private JSONObject loginInformation = new JSONObject();
+    private final boolean mode;
 
     private final String tableToQuery;
     private final String userName;
+    private final String usernameColumn;
 
     LoginRunnable (MainActivity m, String uname, boolean mode) throws SQLException {
         main = m;
         userName = uname;
+        this.mode = mode;
 
-        if (mode)
+        if (mode) {
             tableToQuery = "EMPLOYER";
-        else
+            usernameColumn = "e_username";
+        }
+
+        else {
             tableToQuery = "APPLICANT";
+            usernameColumn = "a_username";
+        }
+
     }
 
     @Override
@@ -35,20 +44,35 @@ public class LoginRunnable implements Runnable {
             Connection conn = DriverManager.getConnection(azureURL, "kingSeal@sealsearch", "Password1");
 
             Statement queryStatement = conn.createStatement();
-            QUERY = String.format(QUERY, tableToQuery, userName);
+            QUERY = String.format(QUERY, tableToQuery, usernameColumn, userName);
 
             ResultSet rs = queryStatement.executeQuery(QUERY);
             rs.next();
 
-            try {
+            if (!mode) { //if applicant
+                try {
+                    loginInformation.put("userName", rs.getString("a_username"));
+                    loginInformation.put("firstName", rs.getString("a_fname"));
+                    loginInformation.put("lastName", rs.getString("a_lname"));
+                    loginInformation.put("DOB", rs.getString("a_dob"));
+                } catch (SQLException e) {
+                    main.runOnUiThread(() -> main.invalidLogin());
+                    return;
+                }
+            }
 
-                loginInformation.put("userName", rs.getString("a_username"));
-                loginInformation.put("firstName", rs.getString("a_fname"));
-                loginInformation.put("lastName", rs.getString("a_lname"));
-                loginInformation.put("DOB", rs.getString("a_dob"));
-            } catch (SQLException e) {
-                main.runOnUiThread(() -> main.invalidLogin());
-                return;
+            else { //if employer
+                try {
+                    loginInformation.put("companyName", rs.getString("e_companyName"));
+                    loginInformation.put("username", rs.getString("e_username"));
+                    loginInformation.put("firstName", rs.getString("e_fname"));
+                    loginInformation.put("lastName", rs.getString("e_lname"));
+                    loginInformation.put("imageBytes", rs.getString("e_androidEncode"));
+                    loginInformation.put("DOB", rs.getString("e_dob"));
+                } catch (SQLException e) {
+                    main.runOnUiThread(() -> main.invalidLogin());
+                    return;
+                }
             }
 
 
